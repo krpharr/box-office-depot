6bbfa69e3078d5c943b071e541b7cb1bee9abedb
Lorenzo T. Tuppince <lorenzot.tuppince@Macs-MacBook-Pro.local>
2019-12-19 21:26:38 -0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-window.addEventListene
+function queryGeoLocation(){
+    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=23228&key=AIzaSyAD8wycqgshyqwS8pWhA1GF8_7XoJPR8xA";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+    });
+}

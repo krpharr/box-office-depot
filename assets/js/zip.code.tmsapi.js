3ef641cdf9e3c27832bc44b5999f49fd5060161f
rpharr <rpharr@richmond.edu>
2019-12-28 14:17:56 -0500

    save

--- assets/js/zip.code.tmsapi.js
+++ assets/js/zip.code.tmsapi.js
@@ -1,3 +1,5 @@
+const fullDaysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 function moviesByZip(zipcode) {
     //
     // Query TMSAPI for all movie showings by zip code and write results to localStarage
@@ -84,4 +86,35 @@ $("#zipcode-submit-ID").on("click", function(event) {
     console.log("event");
     queryGeoLocation($("#zipcode-input-ID").val());
     moviesByZip($("#zipcode-input-ID").val());
+
+    //////////////////////////////////////////////////////////////////
+    /////////////
+    /////////////  test data display for the movie search by zip code "Star Wars: The Rise of Skywalker"
+    let title = "Star Wars: The Rise of Skywalker";
+    container = $("#placeholder-info-ID");
+    container.empty().append($("<h2>").text(title), $("<h4>").text(`${fullDaysOfWeek[moment().weekday()]} ${moment().format("M/D/YYYY")}`));
+    let theaters = sortMovieShowtimesByTheatre(getMovieShowtimes(title));
+    // console.log(theaters);
+    theaters.forEach(theatre => {
+        let div = $("<div class='col s12' style='border: 1px solid black; margin: 8px 0;padding: 8px!important;display: flex;flex-wrap: wrap;'>");
+        let h3 = $("<h3 style='width:100%'>").text(theatre[0].theatre.name);
+        div.append(h3);
+        let row = $("<div class='row center'>");
+        theatre.forEach(showtime => {
+            let showtime_div = $("<div class='col s2 center' style='display: inline; margin: 2px; padding: 4px;border: 1px solid black;'>");
+            var fandango;
+            if (showtime.ticketURI) {
+                fandango = $(`<a href='${showtime.ticketURI}' target='_blank' style='margin-left: 4px;'>${moment(showtime.dateTime).format("h:mm a")}</a>`);
+            } else {
+                fandango = $("<p style='display: inline; margin-left: 8px;'>").text(moment(showtime.dateTime).format("h:mm a"));
+            }
+            showtime_div.append(fandango);
+            row.append(showtime_div);
+        });
+        div.append(row);
+        container.append(div);
+    });
+    //////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
+
 });
